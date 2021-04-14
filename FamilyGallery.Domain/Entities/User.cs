f4b1c5256ad9a8ebc9758cb710487a809f24bf35
Emem Isaac <me@ememisaac.com>
2021-04-14 23:48:20 +0100

    Create the user enttiy

---
+++ FamilyGallery.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FamilyGallery.Domain.Common;
+
+
+namespace FamilyGallery.Domain.Entities
+{
+    public class User : AuditedEntity<Guid>
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
