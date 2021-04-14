f39144fd56f5c8e4554a219f6fdef8ac4c9d758c
Emem Isaac <me@ememisaac.com>
2021-04-14 23:48:33 +0100

    Create the family member entity

---
+++ FamilyGallery.Domain/Entities/FamilyMember.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using FamilyGallery.Domain.Common;
+
+
+namespace FamilyGallery.Domain.Entities
+{
+    class FamilyMember : AuditedEntity<Guid>
+    {
+        public Guid FamilyId { get; set; }
+        
+        public Guid UserId { get; set; }
+    }
+}
