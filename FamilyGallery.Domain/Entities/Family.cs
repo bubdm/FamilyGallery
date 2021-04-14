016261554bffb5a91f07a09445b5057e5f82014f
Emem Isaac <me@ememisaac.com>
2021-04-14 23:47:49 +0100

    Create the family entity

---
+++ FamilyGallery.Domain/Entities/Family.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using FamilyGallery.Domain.Common;
+
+
+namespace FamilyGallery.Domain.Entities
+{
+    public class Family : AuditedEntity<Guid>
+    {
+        public string Name { get; set; }
+
+        public int CreatorId { get; set; }
+
+        public IReadOnlyList<User> Members { get; set; }
+    }
+}
