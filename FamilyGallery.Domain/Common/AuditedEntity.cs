221f2703e3cb0cb68691f547b0aa221be2700897
Emem Isaac <me@ememisaac.com>
2021-04-14 23:47:01 +0100

    Add base classes common to most entities

---
+++ FamilyGallery.Domain/Common/AuditedEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Domain.Common
+{
+    public class AuditedEntity<T> : Entity<T> where T : struct
+    {
+        public string CreatedBy { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
