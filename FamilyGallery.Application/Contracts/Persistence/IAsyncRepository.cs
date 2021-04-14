6a800cd792cefe5802d6669dbf0cd2de5dd8906c
Emem Isaac <me@ememisaac.com>
2021-04-14 23:53:11 +0100

    Create the generic async repository interface

---
+++ FamilyGallery.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,22 @@
+﻿using FamilyGallery.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T, K> where T : Entity<K> where K : struct
+    {
+        Task<T> GetByIdAsync(K id);
+
+        Task<IReadOnlyList<T>> ListAllAsync();
+
+        Task<T> AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+    }
+}
