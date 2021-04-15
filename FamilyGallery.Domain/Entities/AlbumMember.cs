a1f8c57b8d9e8bc190b73b69387e1425ace14d6f
Emem Isaac <me@ememisaac.com>
2021-04-15 22:02:21 +0100

    Add the AlbumMember entity
    This entity will help us track who has been granted access to an album

---
+++ FamilyGallery.Domain/Entities/AlbumMember.cs
@@ -0,0 +1,16 @@
+﻿using FamilyGallery.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Domain.Entities
+{
+    public class AlbumMember : AuditedEntity<Guid>
+    {
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
