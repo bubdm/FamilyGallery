3350f006f66bee676b907f128e58e68ce512cb22
Emem Isaac <me@ememisaac.com>
2021-04-17 22:55:33 +0100

    Guid automapper on how to map to and from a type

---
+++ FamilyGallery.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using FamilyGallery.Application.Features;
+using FamilyGallery.Application.Features.Families;
+using FamilyGallery.Application.Features.Families.Commands;
+using FamilyGallery.Application.Features.Families.Commands.UpdateFamily;
+using FamilyGallery.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Family, FamilyVm>().ReverseMap();
+            CreateMap<User, UserVm>().ReverseMap();
+            CreateMap<FamilyMember, FamilyMemberVm>().ReverseMap();
+            CreateMap<Family, CreateFamilyCommand>().ReverseMap();        
+            CreateMap<Family, UpdateFamilyCommand>().ReverseMap();
+        }
+    }
+}
