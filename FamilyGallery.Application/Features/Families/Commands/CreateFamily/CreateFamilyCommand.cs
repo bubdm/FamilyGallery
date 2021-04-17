3059db05701fc44a42465f4554fe08cab6cbf219
Emem Isaac <me@ememisaac.com>
2021-04-17 22:53:00 +0100

    Provide the create family command and its handler

---
+++ FamilyGallery.Application/Features/Families/Commands/CreateFamily/CreateFamilyCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Families.Commands
+{
+    public class CreateFamilyCommand : IRequest<Guid>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ FamilyGallery.Application/Features/Families/Commands/CreateFamily/CreateFamilyCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using FamilyGallery.Application.Contracts.Persistence;
+using FamilyGallery.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Families.Commands.CreateFamily
+{
+    public class CreateFamilyCommandHandler : IRequestHandler<CreateFamilyCommand, Guid>
+    {
+        private readonly IFamilyRepository familyRepository;
+        private readonly IMapper mapper;
+
+        public CreateFamilyCommandHandler(IFamilyRepository familyRepository, IMapper mapper)
+        {
+            this.familyRepository = familyRepository;
+            this.mapper = mapper;
+        }
+        public async Task<Guid> Handle(CreateFamilyCommand request, CancellationToken cancellationToken)
+        {
+            var family = mapper.Map<Family>(request);
+            var result = await familyRepository.AddAsync(family);
+            return result.Id;
+        }
+    }
+}
