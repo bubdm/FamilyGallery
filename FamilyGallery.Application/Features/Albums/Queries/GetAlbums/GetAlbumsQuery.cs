337606e6342e4b6cf100a4a8b01814fe5d69a2c6
Emem Isaac <me@ememisaac.com>
2021-04-17 22:51:58 +0100

    Create the get albums query and its handler

---
+++ FamilyGallery.Application/Features/Albums/Queries/GetAlbums/GetAlbumsQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Albums.Queries.GetAlbums
+{
+    public class GetAlbumsQuery : IRequest<List<AlbumVm>>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ FamilyGallery.Application/Features/Albums/Queries/GetAlbums/GetAlbumsQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using FamilyGallery.Application.Contracts.Persistence;
+using MediatR;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Albums.Queries.GetAlbums
+{
+    public class GetAlbumsQueryHandler : IRequestHandler<GetAlbumsQuery, List<AlbumVm>>
+    {
+        private readonly IMapper mapper;
+        private readonly IAlbumRepository albumRepository;
+        private readonly IMediaRepository mediaRepository;
+
+        public GetAlbumsQueryHandler(IMapper mapper, IAlbumRepository albumRepository, IMediaRepository mediaRepository)
+        {
+            this.mapper = mapper;
+            this.albumRepository = albumRepository;
+            this.mediaRepository = mediaRepository;
+        }
+        public async Task<List<AlbumVm>> Handle(GetAlbumsQuery request, CancellationToken cancellationToken)
+        {
+            var albums = await albumRepository.GetByFamilyAsync(request.Id);
+            return mapper.Map<List<AlbumVm>>(albums);
+        }
+    }
+}
