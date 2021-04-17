5a784493572464ebac4d44bdee170649f05d506f
Emem Isaac <me@ememisaac.com>
2021-04-17 22:53:23 +0100

    Create the delete family command and its handler

---
+++ FamilyGallery.Application/Features/Families/Commands/DeleteFamily/DeleteFamilyCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using FamilyGallery.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Families.Commands.DeleteFamily
+{
+    public class DeleteFamilyCommandHandler : IRequestHandler<DeleteFamilyCommand>
+    {
+        private readonly IFamilyRepository familyRepository;
+
+        public DeleteFamilyCommandHandler(IFamilyRepository familyRepository)
+        {
+            this.familyRepository = familyRepository;
+        }
+        public async Task<Unit> Handle(DeleteFamilyCommand request, CancellationToken cancellationToken)
+        {
+            var family = await familyRepository.GetByIdAsync(request.Id);
+            await familyRepository.DeleteAsync(family);
+            return Unit.Value;
+        }
+    }
+}
