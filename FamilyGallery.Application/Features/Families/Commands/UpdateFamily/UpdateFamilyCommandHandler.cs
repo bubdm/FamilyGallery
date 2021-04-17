c87da765df0b15ecd353e0af85fb800f1fee4423
Emem Isaac <me@ememisaac.com>
2021-04-17 22:53:49 +0100

    Create the update family command and its handler

---
+++ FamilyGallery.Application/Features/Families/Commands/UpdateFamily/UpdateFamilyCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using FamilyGallery.Application.Contracts.Persistence;
+using FamilyGallery.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Families.Commands.UpdateFamily
+{
+    public class UpdateFamilyCommandHandler : IRequestHandler<UpdateFamilyCommand>
+    {
+        private readonly IMapper mapper;
+        private readonly IFamilyRepository familyRepository;
+
+        public UpdateFamilyCommandHandler(IMapper mapper, IFamilyRepository familyRepository)
+        {
+            this.mapper = mapper;
+            this.familyRepository = familyRepository;
+        }
+        public async Task<Unit> Handle(UpdateFamilyCommand request, CancellationToken cancellationToken)
+        {
+            var family = await familyRepository.GetByIdAsync(request.Id);
+            mapper.Map(request, family, typeof(UpdateFamilyCommand), typeof(Family));
+            await familyRepository.UpdateAsync(family);
+            return Unit.Value;
+        }
+    }
+}
