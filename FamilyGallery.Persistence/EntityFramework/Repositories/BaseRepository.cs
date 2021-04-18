a422eb095fc5e79e8ef0d39c98ef2db5cb27def2
Emem Isaac <me@ememisaac.com>
2021-04-18 19:53:01 +0100

    Implement the base repository

---
+++ FamilyGallery.Persistence/EntityFramework/Repositories/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using FamilyGallery.Application.Contracts.Persistence;
+using FamilyGallery.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Persistence.EntityFramework.Repositories
+{
+    public class BaseRepository<T> : IAsyncRepository<T> where T : AuditedEntity
+    {
+        private readonly FamilyGalleryDbContext dbContext;
+
+        public BaseRepository(FamilyGalleryDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await dbContext.Set<T>().AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            dbContext.Set<T>().Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task<T> GetByIdAsync(Guid id)
+        {
+            return await dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            dbContext.Entry(entity).State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
