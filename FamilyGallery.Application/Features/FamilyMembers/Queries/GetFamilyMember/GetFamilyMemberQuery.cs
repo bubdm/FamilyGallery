05261706c759f145bbc25f19d6a04ce1853c017c
Emem Isaac <me@ememisaac.com>
2021-04-18 14:51:14 +0100

    Add Get Family Member query and  its handler

---
+++ FamilyGallery.Application/Features/FamilyMembers/Queries/GetFamilyMember/GetFamilyMemberQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.FamilyMembers.Queries.GetFamilyMember
+{
+    public class GetFamilyMemberQuery : IRequest<FamilyMemberVm>
+    {
+        public Guid Id { get; internal set; }
+    }
+}
---
+++ FamilyGallery.Application/Features/FamilyMembers/Queries/GetFamilyMember/GetFamilyMemberQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using FamilyGallery.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.FamilyMembers.Queries.GetFamilyMember
+{
+    public class GetFamilyMemberQueryHandler : IRequestHandler<GetFamilyMemberQuery, FamilyMemberVm>
+    {
+        private readonly IFamilyMemberRepository familyMemberRepository;
+        private readonly GetFamilyMemberQuery getFamilyMemberQuery;
+        private readonly IMapper mapper;
+
+        public GetFamilyMemberQueryHandler(IFamilyMemberRepository familyMemberRepository, GetFamilyMemberQuery getFamilyMemberQuery, IMapper mapper)
+        {
+            this.familyMemberRepository = familyMemberRepository;
+            this.getFamilyMemberQuery = getFamilyMemberQuery;
+            this.mapper = mapper;
+        }
+        public async Task<FamilyMemberVm> Handle(GetFamilyMemberQuery request, CancellationToken cancellationToken)
+        {
+            var familyMember = await familyMemberRepository.GetByIdAsync(request.Id);
+            return mapper.Map<FamilyMemberVm>(familyMember);
+        }
+    }
+}
