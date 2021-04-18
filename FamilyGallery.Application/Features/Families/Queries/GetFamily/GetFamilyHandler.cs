0563737ce717dda633e93770c757d2848625339e
Emem Isaac <me@ememisaac.com>
2021-04-18 14:53:48 +0100

    Create the Get Family Query and its handler

---
+++ FamilyGallery.Application/Features/Families/Queries/GetFamily/GetFamilyHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using FamilyGallery.Application.Contracts.Persistence;
+using FamilyGallery.Application.Features.Users;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Families.GetFamily
+{
+    public class GetFamilyHandler : IRequestHandler<GetFamilyQuery, FamilyVm>
+    {
+        private readonly IMapper mapper;
+        private readonly IFamilyRepository familyRepository;
+        private readonly IFamilyMemberRepository familyMemberRepository;
+
+        public GetFamilyHandler(IMapper mapper, IFamilyRepository familyRepository, IFamilyMemberRepository familyMemberRepository)
+        {
+            this.mapper = mapper;
+            this.familyRepository = familyRepository;
+            this.familyMemberRepository = familyMemberRepository;
+        }
+        public async Task<FamilyVm> Handle(GetFamilyQuery request, CancellationToken cancellationToken)
+        {
+            var family = await familyRepository.GetByIdAsync(request.Id);
+            var familyVm = mapper.Map<FamilyVm>(family);
+            var members = familyMemberRepository.GetByFamily(request.Id);
+            familyVm.Members = mapper.Map<List<UserVm>>(members);
+            return familyVm;
+        }
+    }
+}
