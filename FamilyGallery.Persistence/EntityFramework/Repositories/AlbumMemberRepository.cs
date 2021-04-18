02decc66a7a3197553f88a340a14e79ea3457272
Emem Isaac <me@ememisaac.com>
2021-04-18 19:53:24 +0100

    Implement the album member repository

---
+++ FamilyGallery.Persistence/EntityFramework/Repositories/AlbumMemberRepository.cs
@@ -0,0 +1,17 @@
+﻿using FamilyGallery.Application.Contracts.Persistence;
+using FamilyGallery.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Persistence.EntityFramework.Repositories
+{
+    public class AlbumMemberRepository : BaseRepository<AlbumMember>, IAlbumMemberRepository
+    {
+        public AlbumMemberRepository(FamilyGalleryDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
