1156c8e65f21531cf9d907c90d29ae7637cea921
Emem Isaac <me@ememisaac.com>
2021-04-18 18:26:25 +0100

    create a base configuration for all audited entities

---
+++ FamilyGallery.Persistence/EntityFramework/Configurations/AuditedEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using FamilyGallery.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Persistence.EntityFramework.Configurations
+{
+    public class AuditedEntityConfiguration : IEntityTypeConfiguration<AuditedEntity>
+    {
+        public void Configure(EntityTypeBuilder<AuditedEntity> builder)
+        {
+            builder.Property(e => e.CreatedBy).IsRequired().HasMaxLength(50);
+            builder.Property(e => e.LastModifiedBy).HasMaxLength(50);
+        }
+    }
+}
