1f4197d0080664573d1ef2418dd2f35b502463b7
Emem Isaac <me@ememisaac.com>
2021-04-18 19:54:26 +0100

    Implement the family repository

---
+++ FamilyGallery.Persistence/EntityFramework/Repositories/FamilyRepository.cs
@@ -0,0 +1,20 @@
+﻿using FamilyGallery.Application.Contracts.Persistence;
+using FamilyGallery.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Persistence.EntityFramework.Repositories
+{
+    public class FamilyRepository : BaseRepository<Family>, IFamilyRepository
+    {
+        private readonly FamilyGalleryDbContext dbContext;
+
+        public FamilyRepository(FamilyGalleryDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
