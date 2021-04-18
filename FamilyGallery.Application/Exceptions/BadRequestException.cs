dfccbaf09bbbdb5125bf75db5692a4b0c6b0e75b
Emem Isaac <me@ememisaac.com>
2021-04-18 15:00:20 +0100

    Create custom exceptions

---
+++ FamilyGallery.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Exceptions
+{
+    public class BadRequestException : ApplicationException
+    {
+        public BadRequestException(string message) : base(message)
+        { }
+    }
+
+}
