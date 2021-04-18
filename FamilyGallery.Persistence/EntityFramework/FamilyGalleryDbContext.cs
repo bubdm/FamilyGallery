c2ce4ec33a7d8ea9f8a9584424eaff0987b27f7a
Emem Isaac <me@ememisaac.com>
2021-04-18 18:11:03 +0100

    Create the FamilyGalleries db context

---
+++ FamilyGallery.Persistence/EntityFramework/FamilyGalleryDbContext.cs
@@ -0,0 +1,32 @@
+﻿using FamilyGallery.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Persistence.EntityFramework
+{
+    public class FamilyGalleryDbContext : DbContext
+    {
+        public FamilyGalleryDbContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Family> Families { get; set; }
+
+        public DbSet<FamilyMember> FamilyMembers { get; set; }
+
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<File> Files { get; set; }
+
+        public DbSet<AlbumMember> AlbumMembers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(FamilyGalleryDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
