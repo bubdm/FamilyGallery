24291c4fa4246088f3801fbe19a1cf4642717415
Emem Isaac <me@ememisaac.com>
2021-04-18 19:54:56 +0100

    Implement the user repository

---
+++ FamilyGallery.Persistence/EntityFramework/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using FamilyGallery.Application.Contracts.Persistence;
+using FamilyGallery.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Persistence.EntityFramework.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(FamilyGalleryDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
