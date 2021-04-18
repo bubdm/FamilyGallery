8606500c3d5de4393927841ebdc5ebc734d2a506
Emem Isaac <me@ememisaac.com>
2021-04-18 19:53:41 +0100

    Implement the album repository

---
+++ FamilyGallery.Persistence/EntityFramework/Repositories/AlbumRepository.cs
@@ -0,0 +1,39 @@
+﻿using FamilyGallery.Application.Contracts.Persistence;
+using FamilyGallery.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Persistence.EntityFramework.Repositories
+{
+    public class AlbumRepository : BaseRepository<Album>, IAlbumRepository
+    {
+        private readonly FamilyGalleryDbContext dbContext;
+
+        public AlbumRepository(FamilyGalleryDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<ICollection<Album>> GetByFamilyAsync(Guid familyId)
+        {
+            var result = dbContext.Albums
+                .Join(
+                    dbContext.FamilyMembers,
+                    album => album.CreatorId,
+                    member => member.UserId,
+                    (album, member) => new
+                    {
+                        Album = album,
+                        FamilyId = member.FamilyId
+                    }).Where(x => x.FamilyId == familyId)
+                .Select(x => x.Album)
+                .ToListAsync();
+
+            return await result;
+        }
+    }
+}
