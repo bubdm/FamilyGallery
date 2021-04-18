200e255c6afa1b5b5cb0e60d6df8f8870b200c16
Emem Isaac <me@ememisaac.com>
2021-04-18 14:53:29 +0100

    Create the Get Families query and its handler

---
+++ FamilyGallery.Application/Features/Families/Queries/GetFamilies/GetFamiliesQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using FamilyGallery.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FamilyGallery.Application.Features.Families;
+
+namespace FamilyGallery.Application.Features.Families.GetFamilies
+{
+    public class GetFamiliesQueryHandler : IRequestHandler<GetFamiliesQuery, List<FamilyVm>>
+    {
+        private readonly IMapper mapper;
+        private readonly IFamilyRepository familyRepository;
+
+        public GetFamiliesQueryHandler(IMapper mapper, IFamilyRepository familyRepository)
+        {
+            this.mapper = mapper;
+            this.familyRepository = familyRepository;
+        }
+        public async Task<List<FamilyVm>> Handle(GetFamiliesQuery request, CancellationToken cancellationToken)
+        {
+            var families = await familyRepository.ListAllAsync();
+            return mapper.Map<List<FamilyVm>>(families);
+        }
+    }
+}
