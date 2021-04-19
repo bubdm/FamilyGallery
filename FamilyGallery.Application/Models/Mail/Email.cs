d028605435c9b71b95580f989b642c2163fa394a
Emem Isaac <me@ememisaac.com>
2021-04-19 03:32:11 +0100

    Create Email model

---
+++ FamilyGallery.Application/Models/Mail/Email.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Models.Mail
+{
+    public class Email
+    {
+        public Email()
+        {
+            CC = new List<string>();
+            BCC = new List<string>();
+        }
+
+        public string To { get; set; }
+        
+        public List<string> CC { get; set; }
+        
+        public List<string> BCC { get; set; }
+        
+        public string Subject { get; set; }
+        
+        public string Body { get; set; }
+    }
+}
