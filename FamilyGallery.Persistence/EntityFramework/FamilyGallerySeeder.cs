3c3878b143fd341804df0c79cbed011772650934
Emem Isaac <me@ememisaac.com>
2021-04-19 03:39:43 +0100

    Create an EF data seeder

---
+++ FamilyGallery.Persistence/EntityFramework/FamilyGallerySeeder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Persistence.EntityFramework
+{
+    public class FamilyGallerySeeder
+    {
+        private readonly FamilyGalleryDbContext context;
+
+        public FamilyGallerySeeder(FamilyGalleryDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Seed()
+        {
+            context.Database.EnsureCreated();
+            if (!context.Families.Any())
+            {
+                //seed families                
+            }
+        }
+    }
+}
