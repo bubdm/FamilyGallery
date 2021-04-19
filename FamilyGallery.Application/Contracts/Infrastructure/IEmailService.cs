783b87d717eaa66f97b6bf0ad42db6c3426a7531
Emem Isaac <me@ememisaac.com>
2021-04-19 03:32:53 +0100

    Create emailing interface

---
+++ FamilyGallery.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using FamilyGallery.Application.Models.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
