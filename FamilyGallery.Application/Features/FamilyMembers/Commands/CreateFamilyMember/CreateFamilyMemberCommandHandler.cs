550c2d8bc14ca26813e8e80a5c8e87fe7e665eb5
Emem Isaac <me@ememisaac.com>
2021-04-21 04:51:48 +0100

    Adds logic to handle the add family member command
    A user is created if one doesn't exist because each family member must be a user. Then fires an invite to that user

---
+++ FamilyGallery.Application/Features/FamilyMembers/Commands/CreateFamilyMember/CreateFamilyMemberCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using FamilyGallery.Application.Contracts.Infrastructure;
+using FamilyGallery.Application.Contracts.Persistence;
+using FamilyGallery.Application.Models.Mail;
+using FamilyGallery.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.FamilyMembers.Commands.CreateFamilyMember
+{
+    public class CreateFamilyMemberCommandHandler : IRequestHandler<CreateFamilyMemberCommand, Guid>
+    {
+        private readonly IFamilyMemberRepository familyMemberRepository;
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+        private readonly IInviteService inviteService;
+
+        public CreateFamilyMemberCommandHandler(IFamilyMemberRepository familyMemberRepository, IUserRepository userRepository, IMapper mapper, IInviteService inviteService)
+        {
+            this.familyMemberRepository = familyMemberRepository;
+            this.userRepository = userRepository;
+            this.mapper = mapper;
+            this.inviteService = inviteService;
+        }
+
+        public async Task<Guid> Handle(CreateFamilyMemberCommand request, CancellationToken cancellationToken)
+        {
+            var familyMember = mapper.Map<FamilyMember>(request);
+            var user = await userRepository.FindByEmail(request.Email);
+            if (user == null)
+            {
+                user = mapper.Map<User>(request);
+                user = await userRepository.AddAsync(user);
+            }
+            familyMember.UserId = user.Id;
+            var result = await familyMemberRepository.AddAsync(familyMember);
+            await inviteService.InviteFamilyMember(request);
+            return result.Id;
+        }
+    }
+}
