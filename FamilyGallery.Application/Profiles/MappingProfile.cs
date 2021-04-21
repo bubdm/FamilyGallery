eb20692f33ab0423f73152bc271466b3de6726a7
Emem Isaac <me@ememisaac.com>
2021-04-21 04:52:07 +0100

    Adds additioanal mappings

--- FamilyGallery.Application/Profiles/MappingProfile.cs
+++ FamilyGallery.Application/Profiles/MappingProfile.cs
@@ -3,6 +3,7 @@ using FamilyGallery.Application.Features;
 using FamilyGallery.Application.Features.Families;
 using FamilyGallery.Application.Features.Families.Commands;
 using FamilyGallery.Application.Features.Families.Commands.UpdateFamily;
+using FamilyGallery.Application.Features.FamilyMembers.Commands.CreateFamilyMember;
 using FamilyGallery.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,8 @@ namespace FamilyGallery.Application.Profiles
             CreateMap<FamilyMember, FamilyMemberVm>().ReverseMap();
             CreateMap<Family, CreateFamilyCommand>().ReverseMap();        
             CreateMap<Family, UpdateFamilyCommand>().ReverseMap();
+            CreateMap<FamilyMember, CreateFamilyMemberCommand>().ReverseMap();
+            CreateMap<User, CreateFamilyMemberCommand>().ReverseMap();
         }
     }
 }
