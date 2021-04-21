df0219f41c7432d879e9d2bc254a2dd632fb379c
Emem Isaac <me@ememisaac.com>
2021-04-21 04:45:32 +0100

    Create a service that constructs and fires off notification messages
    For example family member invitation messages

---
+++ FamilyGallery.Application/Contracts/Infrastructure/IInviteService.cs
@@ -0,0 +1,14 @@
+﻿using FamilyGallery.Application.Features.FamilyMembers.Commands.CreateFamilyMember;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Contracts.Infrastructure
+{
+    public interface IInviteService
+    {
+        Task<bool> InviteFamilyMember(CreateFamilyMemberCommand createFamilyMember);
+    }
+}
