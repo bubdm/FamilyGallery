63ed65339ef1aee42f972c5d70b4d23f1f0752f2
Emem Isaac <me@ememisaac.com>
2021-05-02 00:11:38 +0100

    Add two methods
    IsAlbumMember and DeleteByAlbum

--- FamilyGallery.Application/Contracts/Persistence/IAlbumMemberRepository.cs
+++ FamilyGallery.Application/Contracts/Persistence/IAlbumMemberRepository.cs
@@ -1,9 +1,12 @@
 ﻿using FamilyGallery.Domain.Entities;
 using System;
+using System.Threading.Tasks;
 
 namespace FamilyGallery.Application.Contracts.Persistence
 {
     public interface IAlbumMemberRepository : IAsyncRepository<AlbumMember>
     {
+        Task<bool> IsAlbumMember(Guid albumId, Guid userId);
+        Task DeleteByAlbumId(Guid albumId);
     }
 }
