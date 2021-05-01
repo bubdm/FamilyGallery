8cb8136330d33b2fa2e19b27dc7b4a61089a72ed
Emem Isaac <me@ememisaac.com>
2021-05-01 23:52:55 +0100

    Registers services in the infrastructure project

---
+++ FamilyGallery.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using FamilyGallery.Application.Contracts.Infrastructure;
+using FamilyGallery.Application.Models.Mail;
+using FamilyGallery.Infrastructure.Messaging.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings").Bind);
+            services.AddTransient<IEmailService, EmailService>();
+            services.AddTransient<IInviteService, InviteService>();
+            return services;
+        }
+    }
+}
