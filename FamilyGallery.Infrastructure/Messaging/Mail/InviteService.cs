60ec0ce7dfc4470dc7d7f6f262fdc0cf971631b3
Emem Isaac <me@ememisaac.com>
2021-05-01 23:55:31 +0100

    Implements the Invite Service

---
+++ FamilyGallery.Infrastructure/Messaging/Mail/InviteService.cs
@@ -0,0 +1,33 @@
+﻿using FamilyGallery.Application.Contracts.Infrastructure;
+using FamilyGallery.Application.Features.FamilyMembers.Commands.CreateFamilyMember;
+using FamilyGallery.Application.Models.Mail;
+using FamilyGallery.Domain.Entities;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Infrastructure.Messaging.Mail
+{
+    public class InviteService : IInviteService
+    {
+        private readonly EmailTemplates emailTemplates;
+        private readonly IEmailService emailService;
+
+        public InviteService(IOptions<EmailTemplates> emailTemplates, IEmailService emailService)
+        {
+            this.emailTemplates = emailTemplates.Value;
+            this.emailService = emailService;
+        }
+
+        public Task<bool> InviteFamilyMember(CreateFamilyMemberCommand createFamilyMember)
+        {
+            var email = new Email()
+            {
+                Name = createFamilyMember.Email,
+                EmailAddress = createFamilyMember.FirstName,
+                Body = emailTemplates.FamilyMemberInvite.Body,
+                Subject = emailTemplates.FamilyMemberInvite.Subject
+            };
+            return emailService.Send(email);
+        }
+    }
+}
