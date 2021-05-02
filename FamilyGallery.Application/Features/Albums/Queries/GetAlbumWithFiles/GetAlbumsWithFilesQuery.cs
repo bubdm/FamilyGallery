682a60a04f7f193cae0702c85b92d3aae9ee4f39
Emem Isaac <me@ememisaac.com>
2021-05-02 02:18:00 +0100

    create the get albums with files query handler

---
+++ FamilyGallery.Application/Features/Albums/Queries/GetAlbumWithFiles/GetAlbumsWithFilesQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace FamilyGallery.Application.Features.Albums.Queries.GetAlbumWithFiles
+{
+    public class GetAlbumWithFilesQuery : IRequest<GetAlbumWithFilesQueryResponse>
+    {
+        public Guid UserId { get; set; }
+
+        public Guid AlbumId { get; set; }
+
+        public int NumberOfFiles { get; set; }
+
+        public int Page { get; set; }
+    }
+}
