a41c30bce838e7d48f09de4f0496a31b18f2e75e
Emem Isaac <me@ememisaac.com>
2021-05-02 02:19:15 +0100

    Add the create family command handler

--- FamilyGallery.Application/Features/Families/Commands/CreateFamily/CreateFamilyCommandHandler.cs
+++ FamilyGallery.Application/Features/Families/Commands/CreateFamily/CreateFamilyCommandHandler.cs
@@ -3,36 +3,35 @@ using FamilyGallery.Application.Contracts.Persistence;
 using FamilyGallery.Application.Exceptions;
 using FamilyGallery.Domain.Entities;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace FamilyGallery.Application.Features.Families.Commands.CreateFamily
 {
-    public class CreateFamilyCommandHandler : IRequestHandler<CreateFamilyCommand, Guid>
+    public class CreateFamilyCommandHandler : IRequestHandler<CreateFamilyCommand, CreateFamilyCommandResponse>
     {
         private readonly IFamilyRepository familyRepository;
         private readonly IMapper mapper;
+        private readonly IFamilyMemberRepository familyMemberRepository;
 
-        public CreateFamilyCommandHandler(IFamilyRepository familyRepository, IMapper mapper)
+        public CreateFamilyCommandHandler(IFamilyRepository familyRepository, IMapper mapper, IFamilyMemberRepository familyMemberRepository)
         {
             this.familyRepository = familyRepository;
             this.mapper = mapper;
+            this.familyMemberRepository = familyMemberRepository;
         }
-        public async Task<Guid> Handle(CreateFamilyCommand request, CancellationToken cancellationToken)
+        public async Task<CreateFamilyCommandResponse> Handle(CreateFamilyCommand request, CancellationToken cancellationToken)
         {
             var validator = new CreateFamilyCommandValidator();
             var validationResult = await validator.ValidateAsync(request);
             if (!validationResult.IsValid)
             {
-                throw new ValidationException(validationResult);
+                return new CreateFamilyCommandResponse { IsSuccessful = false, Message = validationResult.ToString() };
             }
             var family = mapper.Map<Family>(request);
-            var result = await familyRepository.AddAsync(family);
-            return result.Id;
+            family = await familyRepository.AddAsync(family);
+            await familyMemberRepository.AddAsync(new FamilyMember { FamilyId = family.Id, CreatorId = family.CreatorId, UserId = request.CreatorId });
+            return new CreateFamilyCommandResponse { Data = mapper.Map<FamilyVm>(family), IsSuccessful = true };
         }
     }
 }
