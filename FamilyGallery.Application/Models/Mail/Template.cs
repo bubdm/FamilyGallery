f44414dea6d597c5e623a3cb20f30854a18547f1
Emem Isaac <me@ememisaac.com>
2021-05-02 15:18:34 +0100

    Define the basic structure of an email template

---
+++ FamilyGallery.Application/Models/Mail/Template.cs
@@ -0,0 +1,8 @@
+﻿namespace FamilyGallery.Application.Models.Mail
+{
+    public class Template
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
