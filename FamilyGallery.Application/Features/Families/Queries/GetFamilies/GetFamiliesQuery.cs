8028526ff2003def89d03d977323bc1f8d58453b
Emem Isaac <me@ememisaac.com>
2021-05-02 02:26:04 +0100

    Add the get families query

--- FamilyGallery.Application/Features/Families/Queries/GetFamilies/GetFamiliesQuery.cs
+++ FamilyGallery.Application/Features/Families/Queries/GetFamilies/GetFamiliesQuery.cs
@@ -5,9 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FamilyGallery.Application.Features.Families.GetFamilies
+namespace FamilyGallery.Application.Features.Families.Queries.GetFamilies
 {
     public class GetFamiliesQuery : IRequest<List<FamilyVm>>
     {
+        public Guid UserId { get; set; }
     }
 }
