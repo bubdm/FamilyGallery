02f26b94ae49bc0763d3dc88ce32db30b4f981b4
Emem Isaac <me@ememisaac.com>
2021-05-02 15:19:51 +0100

    Add additioinal mappings

--- FamilyGallery.Application/Profiles/MappingProfile.cs
+++ FamilyGallery.Application/Profiles/MappingProfile.cs
@@ -1,15 +1,11 @@
 ﻿using AutoMapper;
 using FamilyGallery.Application.Features;
-using FamilyGallery.Application.Features.Families;
+using FamilyGallery.Application.Features.Albums.Commands.CreateAlbum;
+using FamilyGallery.Application.Features.Albums.Commands.UpdateAlbum;
 using FamilyGallery.Application.Features.Families.Commands;
 using FamilyGallery.Application.Features.Families.Commands.UpdateFamily;
 using FamilyGallery.Application.Features.FamilyMembers.Commands.CreateFamilyMember;
 using FamilyGallery.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FamilyGallery.Application.Profiles
 {
@@ -18,12 +14,18 @@ namespace FamilyGallery.Application.Profiles
         public MappingProfile()
         {
             CreateMap<Family, FamilyVm>().ReverseMap();
-            CreateMap<User, UserVm>().ReverseMap();
-            CreateMap<FamilyMember, FamilyMemberVm>().ReverseMap();
             CreateMap<Family, CreateFamilyCommand>().ReverseMap();        
-            CreateMap<Family, UpdateFamilyCommand>().ReverseMap();
+            CreateMap<Family, UpdateFamilyCommand>().ForMember(uac => uac.UpdaterId, a => a.MapFrom(src => src.CreatorId)).ReverseMap();
+
+            CreateMap<FamilyMember, FamilyMemberVm>().ReverseMap();
             CreateMap<FamilyMember, CreateFamilyMemberCommand>().ReverseMap();
+
+            CreateMap<User, UserVm>().ReverseMap();
             CreateMap<User, CreateFamilyMemberCommand>().ReverseMap();
+
+            CreateMap<Album, AlbumVm>().ReverseMap();
+            CreateMap<Album, CreateAlbumCommand>().ReverseMap();
+            CreateMap<Album, UpdateAlbumCommand>().ForMember(uac => uac.UpdaterId, a => a.MapFrom(src => src.CreatorId) ).ReverseMap();
         }
     }
 }
