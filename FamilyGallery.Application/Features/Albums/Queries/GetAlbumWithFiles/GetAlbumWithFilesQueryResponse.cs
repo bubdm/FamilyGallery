799ebde8020526644d8e2cb73e17e7e327485b5e
Emem Isaac <me@ememisaac.com>
2021-05-02 02:18:16 +0100

    create the get albums with files query response

---
+++ FamilyGallery.Application/Features/Albums/Queries/GetAlbumWithFiles/GetAlbumWithFilesQueryResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Albums.Queries.GetAlbumWithFiles
+{
+    public class GetAlbumWithFilesQueryResponse : TypedResponseBase<AlbumVm>
+    {
+    }
+}
