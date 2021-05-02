59b442ce1e25e27545acab0f6a74e1c7692d9ffc
Emem Isaac <me@ememisaac.com>
2021-05-02 15:27:27 +0100

    Add functionality to Convert Validation Errors To String

---
+++ FamilyGallery.Application/Extensions/ConvertValidationErrorsToString.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.Results;
+using System.Text;
+
+namespace FamilyGallery.Application.Extensions
+{
+    public static class ConvertValidationErrorsToString
+    {
+        public static string Convert(this ValidationResult validationResult)
+        {
+            var stringBuilder = new StringBuilder();
+            validationResult.Errors.ForEach(e => stringBuilder.Append(e.ErrorMessage));
+            return stringBuilder.ToString();
+        }
+    }
+}
