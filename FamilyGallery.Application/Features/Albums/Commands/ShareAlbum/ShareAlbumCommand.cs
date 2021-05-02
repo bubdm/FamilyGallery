50c6236ad269b89abeacaf9dcc43d8f1d76e585e
Emem Isaac <me@ememisaac.com>
2021-05-02 02:06:17 +0100

    create the share album command

---
+++ FamilyGallery.Application/Features/Albums/Commands/ShareAlbum/ShareAlbumCommand.cs
@@ -0,0 +1,21 @@
+﻿using FamilyGallery.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Albums.Commands.ShareAlbum
+{
+    public class ShareAlbumCommand : IRequest<ShareAlbumCommandResponse>
+    {
+        public Guid AlbumId { get; set; }
+
+        public Guid ActorId { get; set; }
+
+        public ShareWith ShareWith { get; set; }
+
+        public List<Guid> UserIds{ get; set; }
+    }
+}
