70dd37f14fa48894882a054e9c41bb411b9722da
Emem Isaac <me@ememisaac.com>
2021-05-02 02:03:52 +0100

    Add the create album validator

---
+++ FamilyGallery.Application/Features/Albums/Commands/CreateAlbum/CreateAlbumCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace FamilyGallery.Application.Features.Albums.Commands.CreateAlbum
+{
+    public class CreateAlbumCommandValidator : AbstractValidator<CreateAlbumCommand>
+    {
+        public CreateAlbumCommandValidator()
+        {
+            RuleFor(f => f.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters");
+
+            RuleFor(f => f.Description)                
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 500 characters");
+        }
+    }
+}
