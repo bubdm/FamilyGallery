fd87be8af9b048a998edcd8bb96e9ad41e2602a0
Emem Isaac <me@ememisaac.com>
2021-05-02 02:34:59 +0100

    Add the update file command response

---
+++ FamilyGallery.Application/Features/Files/Commands/UpdateFile/UpdateFileCommandResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Files.Commands.UpdateFile
+{
+    public class UpdateFileCommandResponse : TypedResponseBase<FileVm>
+    {
+    }
+}
