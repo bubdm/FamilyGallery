b3db1bad62d345418cb4454f88e2e854a53cbdf9
Emem Isaac <me@ememisaac.com>
2021-05-02 02:01:59 +0100

    Add the Create Album command

---
+++ FamilyGallery.Application/Features/Albums/Commands/CreateAlbum/CreateAlbumCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+
+namespace FamilyGallery.Application.Features.Albums.Commands.CreateAlbum
+{
+    public class CreateAlbumCommand : IRequest<CreateAlbumCommandResponse>
+    {
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+
+        public Guid CreatorId { get; set; }
+    }
+}
