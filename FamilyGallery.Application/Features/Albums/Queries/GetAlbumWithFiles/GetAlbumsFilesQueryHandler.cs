7cbfa65f13622a961d3f4c7fbb869b48aabfd3a2
Emem Isaac <me@ememisaac.com>
2021-05-02 02:18:23 +0100

    create the get albums with files query handler

---
+++ FamilyGallery.Application/Features/Albums/Queries/GetAlbumWithFiles/GetAlbumsFilesQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using FamilyGallery.Application.Contracts.Persistence;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Albums.Queries.GetAlbumWithFiles
+{
+    public class GetAlbumWithFilesQueryHandler : IRequestHandler<GetAlbumWithFilesQuery, GetAlbumWithFilesQueryResponse>
+    {
+        private readonly IMapper mapper;
+        private readonly IAlbumRepository albumRepository;
+        private readonly IFileRepository mediaRepository;
+
+        public GetAlbumWithFilesQueryHandler(IMapper mapper, IAlbumRepository albumRepository, IFileRepository mediaRepository)
+        {
+            this.mapper = mapper;
+            this.albumRepository = albumRepository;
+            this.mediaRepository = mediaRepository;
+        }
+        public async Task<GetAlbumWithFilesQueryResponse> Handle(GetAlbumWithFilesQuery request, CancellationToken cancellationToken)
+        {
+            var album = await albumRepository.GetWithFilesAsync(request.AlbumId, request.NumberOfFiles, request.Page);
+            return new GetAlbumWithFilesQueryResponse { 
+                IsSuccessful = true,
+                Data = mapper.Map<AlbumVm>(album)
+            };            
+        }
+    }
+}
