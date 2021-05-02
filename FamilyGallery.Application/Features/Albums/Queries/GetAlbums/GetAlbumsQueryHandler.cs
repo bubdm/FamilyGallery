3313c2df0f09ec02d76c8e9d8a651c442efbc8e0
Emem Isaac <me@ememisaac.com>
2021-05-02 02:10:52 +0100

    create the get albums query handler

--- FamilyGallery.Application/Features/Albums/Queries/GetAlbums/GetAlbumsQueryHandler.cs
+++ FamilyGallery.Application/Features/Albums/Queries/GetAlbums/GetAlbumsQueryHandler.cs
@@ -1,29 +1,32 @@
 ﻿using AutoMapper;
 using FamilyGallery.Application.Contracts.Persistence;
 using MediatR;
-using System.Linq;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace FamilyGallery.Application.Features.Albums.Queries.GetAlbums
 {
-    public class GetAlbumsQueryHandler : IRequestHandler<GetAlbumsQuery, List<AlbumVm>>
+    public class GetAlbumsQueryHandler : IRequestHandler<GetAlbumsQuery, GetAlbumsQueryResponse>
     {
         private readonly IMapper mapper;
         private readonly IAlbumRepository albumRepository;
-        private readonly IMediaRepository mediaRepository;
+        private readonly IFileRepository mediaRepository;
 
-        public GetAlbumsQueryHandler(IMapper mapper, IAlbumRepository albumRepository, IMediaRepository mediaRepository)
+        public GetAlbumsQueryHandler(IMapper mapper, IAlbumRepository albumRepository, IFileRepository mediaRepository)
         {
             this.mapper = mapper;
             this.albumRepository = albumRepository;
             this.mediaRepository = mediaRepository;
         }
-        public async Task<List<AlbumVm>> Handle(GetAlbumsQuery request, CancellationToken cancellationToken)
+        public async Task<GetAlbumsQueryResponse> Handle(GetAlbumsQuery request, CancellationToken cancellationToken)
         {
-            var albums = await albumRepository.GetByFamilyAsync(request.Id);
-            return mapper.Map<List<AlbumVm>>(albums);
+            //todo:validate
+            var albums = await albumRepository.GetByOwnerAsync(request.UserId);
+            return new GetAlbumsQueryResponse {
+                IsSuccessful = true,
+                Data = mapper.Map<List<AlbumVm>>(albums)
+            };            
         }
     }
 }
