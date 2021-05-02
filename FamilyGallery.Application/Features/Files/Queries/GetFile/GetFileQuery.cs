cb6b62fc437f98f9a0ad26b5b501bebc7c5f974c
Emem Isaac <me@ememisaac.com>
2021-05-02 02:35:44 +0100

    Add the get file query

---
+++ FamilyGallery.Application/Features/Files/Queries/GetFile/GetFileQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Files.Queries.GetFile
+{
+    public class GetFileQuery : IRequest<GetFileQueryResponse>
+    {
+        public Guid FileId { get; set; }
+
+        public Guid AlbumId { get; set; }
+
+        public Guid UserId { get; set; }
+    }
+}
