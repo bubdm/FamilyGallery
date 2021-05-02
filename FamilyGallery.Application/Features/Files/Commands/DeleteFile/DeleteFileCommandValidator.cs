21511c4f8b7c85a6d632873cd909053eda0013e3
Emem Isaac <me@ememisaac.com>
2021-05-02 02:33:33 +0100

    Add a delete file command validator

---
+++ FamilyGallery.Application/Features/Files/Commands/DeleteFile/DeleteFileCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using FamilyGallery.Application.Contracts.Persistence;
+using FluentValidation;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Files.Commands.DeleteFile
+{
+    public class DeleteFileCommandValidator : AbstractValidator<DeleteFileCommand>
+    {
+        private readonly IAlbumRepository albumRepository;
+        private readonly IFileRepository fileRepository;
+
+        public DeleteFileCommandValidator(IAlbumRepository albumRepository, IFileRepository fileRepository)
+        {
+            this.albumRepository = albumRepository;
+            this.fileRepository = fileRepository;
+            RuleFor(f => f).MustAsync(UploaderOwnsAlbum).WithMessage("You cannot upload to another's album");
+            RuleFor(f => f).MustAsync(UploaderOwnsFile).WithMessage("You cannot delete another's file");
+        }
+
+        private async Task<bool> UploaderOwnsAlbum(DeleteFileCommand deleteFileCommand, CancellationToken arg2)
+        {
+            return await albumRepository.IsAlbumOwnerAsync(deleteFileCommand.AlbumId, deleteFileCommand.CreatorId);
+        }
+
+        private async Task<bool> UploaderOwnsFile(DeleteFileCommand deleteFileCommand, CancellationToken arg2)
+        {
+            return await fileRepository.IsOwner(deleteFileCommand.FileId, deleteFileCommand.CreatorId);
+        }
+    }
+}
