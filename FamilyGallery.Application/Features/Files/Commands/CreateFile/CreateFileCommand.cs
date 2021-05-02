5255b91e0706d81b57bba6183fc4b80e6cb46d3d
Emem Isaac <me@ememisaac.com>
2021-05-02 02:32:12 +0100

    Add a create file command

---
+++ FamilyGallery.Application/Features/Files/Commands/CreateFile/CreateFileCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+
+namespace FamilyGallery.Application.Features.Files.Commands.CreateFile
+{
+    public class CreateFileCommand : IRequest<CreateFileCommandResponse>
+    {
+        public string Path { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string Extension { get; set; }
+
+        public Int64 FileSize { get; set; }
+
+        public Guid CreatorId { get; set; }
+
+        public Guid AlbumId { get; set; }
+
+    }
+}
