fad70a3dcd0bb9777adc59cd7f078bae77815924
Emem Isaac <me@ememisaac.com>
2021-05-02 02:27:34 +0100

    Renamed the Get Family Detail Query

---
+++ FamilyGallery.Application/Features/Families/Queries/GetFamily/GetFamilyQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+
+namespace FamilyGallery.Application.Features.Families.Queries.GetFamily
+{
+    public class GetFamilyQuery : IRequest<FamilyVm>
+    {
+        public Guid FamilyId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
