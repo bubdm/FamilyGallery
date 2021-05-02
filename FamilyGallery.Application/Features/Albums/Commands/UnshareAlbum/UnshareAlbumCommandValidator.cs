9821d55ecef0764c4f44913da4e4068560b1d775
Emem Isaac <me@ememisaac.com>
2021-05-02 02:07:40 +0100

    create the unshare album command validator

---
+++ FamilyGallery.Application/Features/Albums/Commands/UnshareAlbum/UnshareAlbumCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FamilyGallery.Application.Contracts.Persistence;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Albums.Commands.UnshareAlbum
+{
+    public class UnshareAlbumCommandValidator : AbstractValidator<UnshareAlbumCommand>
+    {
+        private readonly IAlbumRepository albumRepository;
+
+        public UnshareAlbumCommandValidator(IAlbumRepository albumRepository)
+        {
+            this.albumRepository = albumRepository;
+            RuleFor(r => r).MustAsync(IsOwner).WithMessage("Only the owner can unshare an album");
+        }
+
+        private async Task<bool> IsOwner(UnshareAlbumCommand command, CancellationToken token)
+        {
+            return await albumRepository.IsAlbumOwnerAsync(command.AlbumId, command.ActorId);
+        }
+    }
+}
