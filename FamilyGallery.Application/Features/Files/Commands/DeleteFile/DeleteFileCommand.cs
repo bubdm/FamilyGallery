299cd0d0bdda77cfb331f0e77d09a471386a6b03
Emem Isaac <me@ememisaac.com>
2021-05-02 02:33:08 +0100

    Add a delete file command

---
+++ FamilyGallery.Application/Features/Files/Commands/DeleteFile/DeleteFileCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System;
+
+namespace FamilyGallery.Application.Features.Files.Commands.DeleteFile
+{
+    public class DeleteFileCommand : IRequest<DeleteFileCommandResponse>
+    {
+        public Guid FileId { get; set; }
+
+        public string Path { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string Extension { get; set; }
+
+        public Int64 FileSize { get; set; }
+
+        public Guid CreatorId { get; set; }
+
+        public Guid AlbumId { get; set; }
+
+    }
+}
