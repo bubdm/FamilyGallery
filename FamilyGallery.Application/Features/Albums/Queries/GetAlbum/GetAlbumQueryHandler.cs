766635f7d7b1ff0df51f547fd1f63951a61ff177
Emem Isaac <me@ememisaac.com>
2021-05-02 02:09:33 +0100

    create the ge album query handler

--- FamilyGallery.Application/Features/Albums/Queries/GetAlbum/GetAlbumQueryHandler.cs
+++ FamilyGallery.Application/Features/Albums/Queries/GetAlbum/GetAlbumQueryHandler.cs
@@ -1,30 +1,37 @@
 ﻿using AutoMapper;
 using FamilyGallery.Application.Contracts.Persistence;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace FamilyGallery.Application.Features.Albums.Queries.GetAlbum
 {
-    public class GetAlbumQueryHandler : IRequestHandler<GetAlbumQuery, AlbumVm>
+    public class GetAlbumQueryHandler : IRequestHandler<GetAlbumQuery, GetAlbumQueryResponse>
     {
         private readonly IMapper mapper;
         private readonly IAlbumRepository albumRepository;
+        private readonly IAlbumMemberRepository albumMemberRepository;
 
-        public GetAlbumQueryHandler(IMapper mapper, IAlbumRepository albumRepository)
+        public GetAlbumQueryHandler(IMapper mapper, IAlbumRepository albumRepository, IAlbumMemberRepository albumMemberRepository)
         {
             this.mapper = mapper;
             this.albumRepository = albumRepository;
+            this.albumMemberRepository = albumMemberRepository;
         }
 
-        public async Task<AlbumVm> Handle(GetAlbumQuery request, CancellationToken cancellationToken)
+        public async Task<GetAlbumQueryResponse> Handle(GetAlbumQuery request, CancellationToken cancellationToken)
         {
-            var album = await albumRepository.GetByIdAsync(request.Id);
-            return mapper.Map<AlbumVm>(album);
+            var validator = new GetAlbumQueryValidator(albumRepository, albumMemberRepository);
+            var validationResult = await validator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {
+                return new GetAlbumQueryResponse { IsSuccessful = false, Message = validationResult.ToString() };
+            }
+            var album = await albumRepository.GetByIdAsync(request.AlbumId);
+            return new GetAlbumQueryResponse { 
+                IsSuccessful = true, 
+                Data = mapper.Map<AlbumVm>(album)
+            }; 
         }
     }
 }
