153d0ca295e9acd8b342d079ea706043d06b030f
Emem Isaac <me@ememisaac.com>
2021-05-02 15:19:09 +0100

    Add typed access to the current known email templates

---
+++ FamilyGallery.Application/Models/Mail/EmailTemplates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Models.Mail
+{
+    public class EmailTemplates
+    {
+        public Template FamilyMemberInvite { get; set; }
+        public Template NewAlbumNotification { get; set; }
+        public Template NewAlbumItemNotification { get; set; }
+    }
+}
