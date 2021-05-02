c1821d1056d69544d414aba346154eaddcd9cbb3
Emem Isaac <me@ememisaac.com>
2021-05-02 02:07:08 +0100

    create the unshare album command

---
+++ FamilyGallery.Application/Features/Albums/Commands/UnshareAlbum/UnshareAlbumCommand.cs
@@ -0,0 +1,18 @@
+﻿using FamilyGallery.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace FamilyGallery.Application.Features.Albums.Commands.UnshareAlbum
+{
+    public class UnshareAlbumCommand : IRequest<UnshareAlbumCommandResponse>
+    {
+        public Guid ActorId { get; set; }
+
+        public Guid AlbumId { get; set; }
+
+        public ShareWith ShareWith { get; set; }
+
+        public List<Guid> FamilyOrUserIds { get; set; }
+    }
+}
