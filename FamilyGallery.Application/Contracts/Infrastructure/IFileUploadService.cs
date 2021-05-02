88e6d860cdb1094f86fde6c89401852d5f9c4bd8
Emem Isaac <me@ememisaac.com>
2021-05-02 15:36:27 +0100

    Add an Interface for file upload services
    We intend to support Azure Storage, AWS S3 and Google Clouds version

---
+++ FamilyGallery.Application/Contracts/Infrastructure/IFileUploadService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Contracts.Infrastructure
+{
+    public interface IFileUploadService
+    {
+        Task<FileUploadResult> Upload(dynamic file);
+    }
+}
