f54782220ad401edacd3277e8b11af6368940ac2
Emem Isaac <me@ememisaac.com>
2021-05-02 15:42:55 +0100

    Add file upload services for the Azure, Google and AWS - unimplmented

---
+++ FamilyGallery.Infrastructure/FileStorage/Amazon/AmazonS3Uploader.cs
@@ -0,0 +1,16 @@
+﻿using FamilyGallery.Application.Contracts.Infrastructure;
+using System;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace FamilyGallery.Infrastructure.FileStorage.Amazon
+{
+    public class AmazonS3Uploader : IFileUploadService
+    {
+        public Task<FileUploadResult> Upload(dynamic file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FamilyGallery.Infrastructure/FileStorage/Google/GoogleFileUploader.cs
@@ -0,0 +1,17 @@
+﻿using FamilyGallery.Application.Contracts.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Infrastructure.FileStorage.Google
+{
+    public class GoogleFileUploader : IFileUploadService
+    {
+        public Task<FileUploadResult> Upload(dynamic file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
