ee41f05188ce82a10827ae87bae0255a007500d3
Emem Isaac <me@ememisaac.com>
2021-05-02 02:20:00 +0100

    Add the delete family command valiator

---
+++ FamilyGallery.Application/Features/Families/Commands/DeleteFamily/DeleteFamilyCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FamilyGallery.Application.Contracts.Persistence;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Families.Commands.DeleteFamily
+{
+    public class DeleteFamilyCommandValidator : AbstractValidator<DeleteFamilyCommand>
+    {
+        private readonly IFamilyRepository familyRepository;
+
+        public DeleteFamilyCommandValidator(IFamilyRepository familyRepository)
+        {
+            this.familyRepository = familyRepository;
+            RuleFor(f => f).MustAsync(DeleterMustBeFamilyCreator).WithMessage("A family can only be deleted by it's creator");
+        }
+
+        private async Task<bool> DeleterMustBeFamilyCreator(DeleteFamilyCommand deleteFamilyCommand, CancellationToken arg2)
+        {
+            var family = await familyRepository.GetByIdAsync(deleteFamilyCommand.FamilyId);
+            return family.CreatorId == deleteFamilyCommand.DeleterId;
+        }
+    }
+}
