6d7bef33ac750dc04b1c37e203e843af4502de3d
Emem Isaac <me@ememisaac.com>
2021-05-02 02:08:59 +0100

    create the ge album query

--- FamilyGallery.Application/Features/Albums/Queries/GetAlbum/GetAlbumQuery.cs
+++ FamilyGallery.Application/Features/Albums/Queries/GetAlbum/GetAlbumQuery.cs
@@ -1,14 +1,12 @@
 ﻿using MediatR;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FamilyGallery.Application.Features.Albums.Queries.GetAlbum
 {
-    public class GetAlbumQuery : IRequest<AlbumVm>
+    public class GetAlbumQuery : IRequest<GetAlbumQueryResponse>
     {
-        public Guid Id { get; set; }
+        public Guid AlbumId { get; set; }
+
+        public Guid UserId { get; set; }
     }
 }
