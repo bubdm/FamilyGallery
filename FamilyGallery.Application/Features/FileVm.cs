33a021c9af0cf2ea53775082e78a40dd7f8a08e4
Emem Isaac <me@ememisaac.com>
2021-05-02 02:36:50 +0100

    Add properties

--- FamilyGallery.Application/Features/FileVm.cs
+++ FamilyGallery.Application/Features/FileVm.cs
@@ -8,14 +8,5 @@ namespace FamilyGallery.Application.Features
 {
     public class FileVm
     {
-        public Guid Id { get; set; }
-
-        public string Path { get; set; }
-     
-        public string ContentType { get; set; }
-
-        public string Extension { get; set; }
-
-        public Int64 FileSize { get; set; }
     }
 }
