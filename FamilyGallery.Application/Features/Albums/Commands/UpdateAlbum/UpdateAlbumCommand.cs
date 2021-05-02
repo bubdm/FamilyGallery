115231f57c5556fdccc2fec40df703949aebbace
Emem Isaac <me@ememisaac.com>
2021-05-02 02:08:08 +0100

    create the update album command

---
+++ FamilyGallery.Application/Features/Albums/Commands/UpdateAlbum/UpdateAlbumCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+
+namespace FamilyGallery.Application.Features.Albums.Commands.UpdateAlbum
+{
+    public class UpdateAlbumCommand : IRequest<UpdateAlbumCommandResponse>
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Guid UpdaterId { get; set; }
+
+        public Guid AlbumId { get; set; }
+    }
+}
