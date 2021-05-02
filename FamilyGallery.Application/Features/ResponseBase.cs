3bd3ee479f12392be36e4a89646c6d34b0f9b548
Emem Isaac <me@ememisaac.com>
2021-05-02 02:02:57 +0100

    Add base classes for the response classes

---
+++ FamilyGallery.Application/Features/ResponseBase.cs
@@ -0,0 +1,9 @@
+﻿namespace FamilyGallery.Application.Features
+{
+    public abstract class ResponseBase
+    {
+        public bool IsSuccessful { get; set; }
+
+        public string Message { get; set; }
+    }
+}
