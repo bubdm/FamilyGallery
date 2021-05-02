2a9da696c282bf30a389ba9472ecd01f0bf36e94
Emem Isaac <me@ememisaac.com>
2021-05-02 02:33:42 +0100

    Add a delete file command handler

---
+++ FamilyGallery.Application/Features/Files/Commands/DeleteFile/DeleteFileCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using FamilyGallery.Application.Contracts.Persistence;
+using FamilyGallery.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Files.Commands.DeleteFile
+{
+    public class DeleteFileCommandHandler : IRequestHandler<DeleteFileCommand, DeleteFileCommandResponse>
+    {
+        private readonly IFileRepository fileRepository;
+        private readonly IMapper mapper;
+        private readonly IAlbumRepository albumRepository;
+
+        public DeleteFileCommandHandler(IFileRepository fileRepository, IMapper mapper, IAlbumRepository albumRepository)
+        {
+            this.fileRepository = fileRepository;
+            this.mapper = mapper;
+            this.albumRepository = albumRepository;
+        }
+
+        public async Task<DeleteFileCommandResponse> Handle(DeleteFileCommand request, CancellationToken cancellationToken)
+        {
+            var response = new DeleteFileCommandResponse();
+            var validator = new DeleteFileCommandValidator(albumRepository, fileRepository);
+            var validationResult = await validator.ValidateAsync(request, default);
+            response.IsSuccessful = validationResult.IsValid;
+            if (validationResult.IsValid)
+            {
+                response.Message = validationResult.ToString();
+                return response;
+            }
+            var file = mapper.Map<File>(request);
+            file = await fileRepository.AddAsync(file);
+            response.IsSuccessful = true;
+            return response;
+        }
+    }
+}
