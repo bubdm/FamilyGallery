c4ae38e88a2c9781cee5be0630a9753848a0041c
Emem Isaac <me@ememisaac.com>
2021-05-02 02:04:27 +0100

    Add the delete album command

---
+++ FamilyGallery.Application/Features/Albums/Commands/DeleteAlbum/DeleteAlbumCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyGallery.Application.Features.Albums.Commands.DeleteAlbum
+{
+    public class DeleteAlbumCommand : IRequest<DeleteAlbumCommandResponse>
+    {
+        public Guid AlbumId { get; set; }
+        public Guid DeleterId { get; set; }
+    }
+}
