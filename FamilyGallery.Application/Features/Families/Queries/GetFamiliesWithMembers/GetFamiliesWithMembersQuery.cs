537b0c6ed7a31667f4fc5a6ffb45a2c67c5b4171
Emem Isaac <me@ememisaac.com>
2021-05-02 02:26:37 +0100

    Add the get families with members query

---
+++ FamilyGallery.Application/Features/Families/Queries/GetFamiliesWithMembers/GetFamiliesWithMembersQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace FamilyGallery.Application.Features.Families.Queries.GetFamiliesWithMembers
+{
+    public class GetFamiliesWithMembersQuery : IRequest<List<FamilyVm>>
+    {
+        public Guid UserId { get; set; }        
+    }
+}
